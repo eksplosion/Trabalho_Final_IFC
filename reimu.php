2cd5570fa9cc6bc3585693d56d684ba585d445a7
Kauan Decarli <kauandecarli@gmail.com>
2021-12-06 10:44:03 -0300

    adiciona easter egg

---
+++ reimu.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <style>
+        body{
+            background-color: #0e0e0e;
+        }
+        h1, p{
+            color: #fff;
+        }
+        div{
+            display: inline-block;
+            margin-left: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Encontraste o Easter Egg! Parabéns!</h1>
+    <img width=400px src="https://steamuserimages-a.akamaihd.net/ugc/857236050643405869/3E14D2AF27AE8D938DF3E743D5710F825C70A23A/">
+    <div>
+        <p>Música:<br><i>Touhou 8: Imperishable Night OST - Maiden's Capriccio ~ Dream Battle</i></p>
+        <audio id="musica" controls autoplay>
+            <source src="https://docs.google.com/uc?export=download&id=1mc5XY09zkeWq_LdM2s_1y2Xtlb_-sZGa" type="audio/mpeg">
+        </audio>
+    <div>
+    <script>
+        document.getElementById("musica").volume = 0.2;
+    </script>
+</body>
+</html>
